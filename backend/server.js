2c47181246561902955d31b35b07780b4152b88c
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-12-14 18:54:31 +0530

    server config

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./src/routes/auth.routes");
-const sweetsRoutes = require("./src/routes/sweets.routes"); // <-- IMPORTANT
+const sweetsRoutes = require("./src/routes/sweets.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Debug
-console.log("Auth routes loaded:", authRoutes);
-console.log("Sweets routes loaded:", sweetsRoutes);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/sweets", sweetsRoutes); // <-- IMPORTANT
+app.use("/api/sweets", sweetsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Sweet Shop API Running...");
